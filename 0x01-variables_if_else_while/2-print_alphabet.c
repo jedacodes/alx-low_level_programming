2994a2a74cc86735f850d59dd535905bc71a4f9c
jedacodes <femiadejobipaulolorunjedalo@gmail.com>
2022-09-10 17:08:16 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,13 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
-
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
